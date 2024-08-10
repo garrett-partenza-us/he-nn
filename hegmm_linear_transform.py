a133c9e1c32ba0072c5c5c572e6d952e56eb2495
Garrett.Partenza <partenza.g@northeastern.edu>
2024-08-09 22:21:58 -0400

    seperated hegmm into two dimensional implementation and linear transformation implementation

--- hegmm_linear_transform.py
+++ hegmm_linear_transform.py
@@ -10,79 +10,6 @@ def shift(row, offset):
     offset = offset % len(row)
     return np.concatenate((row[offset:], row[:offset]))
 
-
-def sigma(arr):
-
-    rows, _ = arr.shape
-    
-    for idx in range(rows):
-        
-        arr[idx] = shift(arr[idx], idx)
-
-    return arr
-
-
-def theta(arr):
-
-    _, cols = arr.shape
-    
-    for idx in range(cols):
-        
-        arr[:,idx] = shift(arr[:,idx], idx)
-
-    return arr
-
-
-def epsilon(arr, size, offset=0):
-
-    rows, _ = arr.shape
-
-    result = np.empty((rows, size))
-
-    for idx in range(rows):
-
-        result[idx] = np.array(list(
-            islice(cycle(shift(arr[idx], offset)), size)
-        ))
-
-    return result
-
-
-def omega(arr, size, offset=0):
-
-    _, cols = arr.shape
-
-    result = np.empty((size, cols))
-
-    for idx in range(cols):
-
-        result[:, idx] = np.array(list(
-            islice(cycle(shift(arr[:, idx], offset)), size)
-        ))
-
-    return result
-
-
-def matmul(a, b):
-
-    assert a.ndim == 2, "matmul error: dimensions must be 2"
-    assert b.ndim == 2, "matmul error: dimensions must be 2"
-    assert a.shape[1] == b.shape[0], "matmul error: dimensions incompatable"
-
-    m, l = a.shape
-    l, n = b.shape
-
-    arr = np.zeros((m, n))
-
-    for k in range(l):
-        lhs = epsilon(sigma(a.copy()), n, offset=k)
-        rhs = omega(theta(b.copy()), m, offset=k)
-        addend = np.multiply(lhs, rhs)
-        arr+=addend
-
-    return arr
-
-
 def sigma_transformation_matrix(m, l):
 
     transformation_matrix = np.zeros((m*l, m*l))
@@ -135,19 +62,19 @@ def omega_transformation_matrix(m, n, l, offset = 0):
 
     return transformation_matrix
 
-def matmul_transformation(a, b):
+def hegmm(a, b):
 
-    assert a.ndim == 2, "matmul error: number of dimensions must be 2"
-    assert b.ndim == 2, "matmul error: number of dimensions must be 2"
+    assert a.ndim == 2, "matmul error: a-matrix number of dimensions must be 2"
+    assert b.ndim == 2, "matmul error: b-matrix number of dimensions must be 2"
     assert a.shape[1] == b.shape[0], "matmul error: dimensions incompatable"
-    
+
     m, l, n = a.shape[0], a.shape[1], b.shape[1]
-    
+
     A = np.matmul(
         sigma_transformation_matrix(m, l),
         a.copy().flatten(order='F')
     )
-    
+
     B = np.matmul(
         theta_transformation_matrix(l, n),
         b.copy().flatten(order='F')
@@ -188,10 +115,8 @@ if __name__ == '__main__':
         b = np.random.rand(l, n)
 
         numpy_matmul = np.matmul(a.copy(), b.copy())
-        hegmm_matmul = matmul_transformation(a.copy(), b.copy())
+        hegmm_matmul = hegmm(a.copy(), b.copy())
 
         assert(np.allclose(numpy_matmul, hegmm_matmul))
 
     print("PASSED")
-
-
